ffc1515187f291e2f3cbeea5b2dae418eb96fc04
Antonin Raathaux <antoninrathaux@gmail.com>
2023-12-04 22:01:56 +0100

    Ajout Page Profil

--- migrations/Version20231203231047.php
+++ migrations/Version20231203231047.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231201105944 extends AbstractMigration
+final class Version20231203231047 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,11 +20,11 @@ final class Version20231201105944 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prix DOUBLE PRECISION NOT NULL, description VARCHAR(255) DEFAULT NULL, stock INT NOT NULL, image VARCHAR(511) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE commande (id INT AUTO_INCREMENT NOT NULL, date DATE NOT NULL, montant_totale DOUBLE PRECISION NOT NULL, statut VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE detail_commande (id INT AUTO_INCREMENT NOT NULL, commandeid_id INT DEFAULT NULL, articleid_id INT DEFAULT NULL, quantite INT NOT NULL, prix_unitaire DOUBLE PRECISION NOT NULL, prix_total DOUBLE PRECISION NOT NULL, INDEX IDX_98344FA6490DEFAF (commandeid_id), INDEX IDX_98344FA69223694D (articleid_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE detail_commande ADD CONSTRAINT FK_98344FA6490DEFAF FOREIGN KEY (commandeid_id) REFERENCES commande (id)');
         $this->addSql('ALTER TABLE detail_commande ADD CONSTRAINT FK_98344FA69223694D FOREIGN KEY (articleid_id) REFERENCES article (id)');
+        $this->addSql('ALTER TABLE user ADD telephone VARCHAR(10) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
@@ -32,8 +32,8 @@ final class Version20231201105944 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE detail_commande DROP FOREIGN KEY FK_98344FA6490DEFAF');
         $this->addSql('ALTER TABLE detail_commande DROP FOREIGN KEY FK_98344FA69223694D');
-        $this->addSql('DROP TABLE article');
-        $this->addSql('DROP TABLE commande');
         $this->addSql('DROP TABLE detail_commande');
+        $this->addSql('DROP TABLE messenger_messages');
+        $this->addSql('ALTER TABLE user DROP telephone');
     }
 }
